67a313c83e46e3967ba554db2b05f6a25dd4578f
ankitpyc <ankitmishra471@gmail.com>
2024-07-11 19:42:52 +0530

    added controller and strucutured the project

--- LinkedIn/src/main/java/org/linkedIn/domain/User.java
+++ LinkedIn/src/main/java/org/linkedIn/domain/User.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User extends Account {
-
+    private Messages messages;
+    private List<User> connections;
 }
